7f23de880305f7ab2bc09ef815002b07aac815a0
chado <a.sale@outlook.fr>
2023-12-30 15:45:54 +0100

    Feat: update type for make properties nullable

--- src/types/CompanyFormType.ts
+++ src/types/CompanyFormType.ts
@@ -1,9 +1,9 @@
 export interface CompanyFormType {
-  name: string;
-  phone: string;
-  address: string;
-  city: string;
-  country: string;
-  siret: string;
+  name: string | null;
+  phone: string | null;
+  address: string | null;
+  city: string | null;
+  country: string | null;
+  siret: string | null;
   user_id: number | null;
 }
