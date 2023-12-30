25bfd27e6bef15e9a70b10f8ab0dc66f4dbf9616
chado <a.sale@outlook.fr>
2023-12-30 15:44:55 +0100

    Feat: add form validation with zod for register company

--- src/components/molecules/company/CompanyModalRegister.vue
+++ src/components/molecules/company/CompanyModalRegister.vue
@@ -4,6 +4,13 @@ import { UserFormType } from "@/types/UserFormType";
 import { CompanyFormType } from "@/types/CompanyFormType";
 import axios from "axios";
 import * as events from "events";
+import {
+  verifCompany,
+  errors,
+  verifEmail,
+  verifyPassword,
+  verifyRepeatPassword,
+} from "@/utils/formValidations";
 
 const props = defineProps({
   toggleRegister: Boolean,
@@ -18,12 +25,12 @@ const user: UserFormType = {
   roles: 5,
 };
 const company: CompanyFormType = {
-  name: "",
-  phone: "",
-  address: "",
-  city: "",
-  country: "",
-  siret: "",
+  name: null,
+  phone: null,
+  address: null,
+  city: null,
+  country: null,
+  siret: null,
   user_id: null,
 };
 
@@ -73,14 +80,14 @@ const register = async (e: events) => {
 </script>
 
 <template>
-  <teleport to="body">
+  <teleport to="nav">
     <div
       @click="emit('update:toggleRegister', false)"
-      class="absolute top-0 left-0 h-screen w-screen bg-black bg-opacity-50 flex justify-center items-center p-2 md:p-8"
+      class="absolute z-[100] top-0 left-0 h-screen w-screen bg-black bg-opacity-50 flex justify-center items-center p-2 md:p-8"
     >
       <div
         @click.stop
-        class="w-11/12 md:w-10/12 lg:w-4/12 rounded-2xl flex flex-col justify-around items-center bg-[#F8F9FAFF]"
+        class="w-11/12 md:w-10/12 lg:w-8/12 xl:w-4/12 rounded-2xl flex flex-col justify-around items-center bg-[#F8F9FAFF]"
       >
         <h2
           class="font-bold uppercase m-4 md:m-8 text-center text-black text-xl md:text-4xl"
@@ -94,29 +101,71 @@ const register = async (e: events) => {
               <input
                 type="email"
                 name="email"
+                @input="verifEmail(user.email)"
                 v-model="user.email"
+                class="focus:outline-none"
+                :class="{
+                  '!border-2 !border-red-600':
+                    errors.email != '' && errors.email != null,
+                  '!border-2 !border-green-600': errors.email === null,
+                }"
                 placeholder="exemple.email@mail.fr"
               />
+              <div
+                v-if="errors.email != '' && errors.email != null"
+                class="border-2 border-red-600 p-2 text-red-600 rounded-md"
+              >
+                <p>{{ errors.email }}</p>
+              </div>
             </div>
             <div class="flex flex-col">
               <label for="password">Mot de passe</label>
               <input
                 type="password"
                 name="password"
+                @input="verifyPassword(user.password)"
                 v-model="user.password"
-                placeholder="Entrez le mot de passe"
+                placeholder="Entre ton mot de passe"
+                class="focus:outline-none"
+                :class="{
+                  '!border-2 !border-red-500':
+                    errors.password != '' && errors.password != null,
+                  '!border-2 !border-green-600': errors.password === null,
+                }"
               />
+              <div
+                v-if="errors.password != '' && errors.password != null"
+                class="border-2 border-red-600 p-2 text-red-600 rounded-md"
+              >
+                <p>{{ errors.password }}</p>
+              </div>
             </div>
             <div class="flex flex-col overflow-hidden">
-              <label for="repeatPassword" class="text-nowrap">
-                Vérification mot de passe
-              </label>
+              <label for="password">Mot de passe</label>
               <input
                 type="password"
-                name="repeatPassword"
+                name="password"
+                @input="
+                  verifyRepeatPassword(user.password, user.repeatPassword)
+                "
                 v-model="user.repeatPassword"
-                placeholder="Entrez à nouveau le mot de passe"
+                placeholder="Entre ton mot de passe"
+                class="focus:outline-none"
+                :class="{
+                  '!border-2 !border-red-500':
+                    errors.repeatPassword != '' &&
+                    errors.repeatPassword != null,
+                  '!border-2 !border-green-600': errors.repeatPassword === null,
+                }"
               />
+              <div
+                v-if="
+                  errors.repeatPassword != '' && errors.repeatPassword != null
+                "
+                class="border-2 border-red-600 p-2 text-red-600 rounded-md"
+              >
+                <p>{{ errors.repeatPassword }}</p>
+              </div>
             </div>
           </form>
           <form class="grid grid-cols-2 gap-2 font-bold">
@@ -125,8 +174,15 @@ const register = async (e: events) => {
               <input
                 type="text"
                 name="name"
+                @input="verifCompany(company)"
                 v-model="company.name"
                 placeholder="Entrez le nom de la société"
+                required="required"
+                :class="{
+                  '!border-2 !border-red-500':
+                    errors.company != null && errors.company != '',
+                  '!border-2 !border-green-600': errors.company === null,
+                }"
               />
             </div>
             <div class="flex flex-col">
@@ -134,8 +190,15 @@ const register = async (e: events) => {
               <input
                 type="text"
                 name="phone"
+                @input="verifCompany(company)"
                 v-model="company.phone"
                 placeholder="Entrez le numéro de téléphone"
+                required="required"
+                :class="{
+                  '!border-2 !border-red-500':
+                    errors.company != null && errors.company != '',
+                  '!border-2 !border-green-600': errors.company === null,
+                }"
               />
             </div>
             <div class="flex flex-col">
@@ -143,8 +206,15 @@ const register = async (e: events) => {
               <input
                 type="text"
                 name="address"
+                @input="verifCompany(company)"
                 v-model="company.address"
                 placeholder="Entrez l'adresse de la société"
+                required="required"
+                :class="{
+                  '!border-2 !border-red-500':
+                    errors.company != null && errors.company != '',
+                  '!border-2 !border-green-600': errors.company === null,
+                }"
               />
             </div>
             <div class="flex flex-col">
@@ -152,8 +222,15 @@ const register = async (e: events) => {
               <input
                 type="text"
                 name="city"
+                @input="verifCompany(company)"
                 v-model="company.city"
                 placeholder="Entrez la ville de la société"
+                required="required"
+                :class="{
+                  '!border-2 !border-red-500':
+                    errors.company != null && errors.company != '',
+                  '!border-2 !border-green-600': errors.company === null,
+                }"
               />
             </div>
             <div class="flex flex-col">
@@ -161,8 +238,15 @@ const register = async (e: events) => {
               <input
                 type="text"
                 name="country"
+                @input="verifCompany(company)"
                 v-model="company.country"
                 placeholder="Entrez le pays de la société"
+                required="required"
+                :class="{
+                  '!border-2 !border-red-500':
+                    errors.company != null && errors.company != '',
+                  '!border-2 !border-green-600': errors.company === null,
+                }"
               />
             </div>
             <div class="flex flex-col">
@@ -170,8 +254,15 @@ const register = async (e: events) => {
               <input
                 type="text"
                 name="siret"
+                @input="verifCompany(company)"
                 v-model="company.siret"
                 placeholder="Entrez le numéro de siret"
+                required="required"
+                :class="{
+                  '!border-2 !border-red-500':
+                    errors.company != null && errors.company != '',
+                  '!border-2 !border-green-600': errors.company === null,
+                }"
               />
             </div>
             <input
@@ -180,6 +271,12 @@ const register = async (e: events) => {
               v-model="company.user_id"
               value=""
             />
+            <div
+              v-if="errors.company != '' && errors.company != null"
+              class="border-2 border-red-600 p-2 text-red-600 rounded-md col-span-2"
+            >
+              <p>{{ errors.company }}</p>
+            </div>
             <div class="col-span-2 py-8 px2 md:p-8">
               <div
                 class="w-full flex flex-col items-center justify-center md:flex-row md:justify-between text-sm md:text-md"
@@ -208,6 +305,9 @@ const register = async (e: events) => {
                 <button
                   @click="register"
                   class="bg-green-900 text-white rounded-3xl p-2 hover:bg-green-950 hover:scale-110 transition ease-in-out"
+                  :disabled="
+                    !Object.values(errors).every((value) => value === null)
+                  "
                 >
                   S'inscrire
                 </button>
@@ -230,7 +330,7 @@ input {
   border: 1px solid black;
   border-radius: 5px;
   padding: 5px;
-  margin: 5px;
+  margin-bottom: 5px;
 }
 
 @media (max-width: 400px) {
