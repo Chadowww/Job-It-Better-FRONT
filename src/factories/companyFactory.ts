7cbe363ffba94194534109d88a54a2173c6d6a01
chado <a.sale@outlook.fr>
2023-12-31 11:02:04 +0100

    Feat: Create file to get responsability aubout entity

---
+++ src/factories/companyFactory.ts
@@ -0,0 +1,11 @@
+import { CompanyFormType } from "@/types/CompanyFormType";
+
+export const company: CompanyFormType = {
+  name: null,
+  phone: null,
+  address: null,
+  city: null,
+  country: null,
+  siret: null,
+  user_id: null,
+};
---
+++ src/factories/userFactory.ts
@@ -0,0 +1,15 @@
+import { UserFormType } from "@/types/UserFormType";
+
+export const companyUser: UserFormType = {
+  email: "",
+  password: "",
+  repeatPassword: "",
+  roles: 5,
+};
+
+export const candidateUser: UserFormType = {
+  email: "",
+  password: "",
+  repeatPassword: "",
+  roles: 3,
+};
