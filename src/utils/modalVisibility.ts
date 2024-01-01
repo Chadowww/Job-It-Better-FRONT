fa584bf2d9c9784def21d33087176725a7f90cff
chado <a.sale@outlook.fr>
2024-01-01 14:03:59 +0100

    Fix: Create new function for get state and toggle the problem was the import about Store who need to bee un vue.js file

--- src/utils/modalVisibility.ts
+++ src/utils/modalVisibility.ts
@@ -1,28 +1,46 @@
 import { computed, ComputedRef } from "vue";
-import { Store, useStore } from "vuex";
 
-const store: Store<any> = useStore();
-export const isCandidateRegistrationModalVisible: ComputedRef = computed(
-  () => store.state.isCandidateRegistrationModalVisible
-);
-export const isCandidateLoginModalVisible: ComputedRef = computed(
-  () => store.state.isCandidateLoginModalVisible
-);
-export const isCompanyLoginModalVisible: ComputedRef = computed(
-  () => store.state.isCompanyLoginModalVisible
-);
-export const isCompanyRegistrationModalVisible: ComputedRef = computed(
-  () => store.state.isCompanyRegistrationModalVisible
-);
-export const toggleCompanyRegistrationModalVisibility = () => {
-  store.dispatch("toggleCompanyRegistrationModalVisibility");
-};
-export const toggleCompanyLoginModalVisibility = () => {
-  store.dispatch("toggleCompanyLoginModalVisibility");
-};
-export const toggleCandidateLoginModalVisibility = () => {
-  store.dispatch("toggleCandidateLoginModalVisibility");
-};
-export const toggleCandidateRegistrationModalVisibility = () => {
-  store.dispatch("toggleCandidateRegistrationModalVisibility");
-};
+interface State {
+  isCandidateRegistrationModalVisible: boolean;
+  isCandidateLoginModalVisible: boolean;
+  isCompanyLoginModalVisible: boolean;
+  isCompanyRegistrationModalVisible: boolean;
+}
+
+interface Store {
+  state: State;
+  dispatch: (arg: string) => void;
+}
+
+const modalStates = [
+  "isCandidateRegistrationModalVisible",
+  "isCandidateLoginModalVisible",
+  "isCompanyLoginModalVisible",
+  "isCompanyRegistrationModalVisible",
+];
+
+const toggleModalActions = [
+  "toggleCompanyRegistrationModalVisibility",
+  "toggleCompanyLoginModalVisibility",
+  "toggleCandidateLoginModalVisibility",
+  "toggleCandidateRegistrationModalVisibility",
+];
+
+export function useModalVisibility(store: Store) {
+  const computedStates = modalStates.reduce((result, key) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    result[key] = computed(() => store.state[key]);
+    return result;
+  }, {} as Record<string, ComputedRef<boolean>>);
+
+  const dispatchers = toggleModalActions.reduce((result, action) => {
+    result[action] = () => store.dispatch(action);
+    return result;
+  }, {} as Record<string, () => void>);
+
+  return {
+    ...computedStates,
+    ...dispatchers,
+  };
+}
