98ad1f02236b8565009b2d330cb6b1dfecef152c
chado <a.sale@outlook.fr>
2024-01-02 12:41:29 +0100

    Add jwt-decode dependency to package.json
    The jwt-decode package has been added as a dependency in the package.json file to decode JWTs. The relevant changes are also reflected in the package-lock.json, including the addition of the package's version, resolved URL and its integrity hash.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,27 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import UserView from "../views/UserView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
+    path: "",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/candidat/profile",
+    name: "CandidateProfile",
+    component: UserView,
+  },
+  {
+    path: "/recruteur/profile",
+    name: "CompanyProfile",
+    component: UserView,
   },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
