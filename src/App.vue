f398c465455ad519fe842a02d8e65e37adff0fd8
chado <a.sale@outlook.fr>
2024-01-02 12:42:04 +0100

    Add NavbarComponent and FooterComponent to App.vue
    The NavbarComponent and FooterComponent have been added to App.vue, and App.vue has been restructured to accommodate these components. This modification allows for better structure, organization and flexibility, as these components are now universally accessible across the application.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <router-view />
+  <div class="min-h-screen w-full flex flex-col justify-between">
+    <navbar-component />
+    <router-view />
+    <FooterComponent />
+  </div>
 </template>
 
 <style lang="scss"></style>
+<script setup lang="ts">
+import FooterComponent from "@/components/organisms/global/FooterComponent.vue";
+import NavbarComponent from "@/components/organisms/global/NavbarComponent.vue";
+</script>
