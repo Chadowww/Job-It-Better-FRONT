4e6b644c88c85304b119bc057e5726e03c50cadc
chado <a.sale@outlook.fr>
2024-01-04 10:37:49 +0100

    Replace SVG with social media components in FooterComponent
    Replaced SVG-based social media icons in the FooterComponent.vue with Vue components. The change simplifies the FooterComponent by removing verbose SVG code and using Vue's single-file component structure, which facilitates better maintainability and reusability. The newly imported components include FacebookButton, LinkedinButton, and TwitterButton.

--- src/components/organisms/global/FooterComponent.vue
+++ src/components/organisms/global/FooterComponent.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import logo from "@/assets/images/newLogo.png";
+import FacebookButton from "@/components/atoms/logo/FacebookButton.vue";
+import LinkedinButton from "@/components/atoms/logo/LinkedinButton.vue";
+import TwitterButton from "@/components/atoms/logo/TwitterButton.vue";
 </script>
 
 <template>
@@ -31,119 +34,9 @@ import logo from "@/assets/images/newLogo.png";
       >
         <h3 class="text-4xl font-bold text-gray-500 mb-4">Suivez-nous</h3>
         <div class="flex transform scale-75 xl:transform-none">
-          <a href="" class="hover:animate-pulse">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              width="100"
-              height="100"
-              viewBox="0 0 48 48"
-            >
-              <linearGradient
-                id="iFd9NrbBteaozGC2ch1MZa_MNRKfKM11gxe_gr1"
-                x1="-10.294"
-                x2="50.002"
-                y1="-10.294"
-                y2="50.002"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop offset="0" stop-color="#262626" stop-opacity="0"></stop>
-                <stop offset="1" stop-color="#262626" stop-opacity=".8"></stop>
-              </linearGradient>
-              <path
-                fill="url(#iFd9NrbBteaozGC2ch1MZa_MNRKfKM11gxe_gr1)"
-                d="M36,6H12c-3.314,0-6,2.686-6,6v24c0,3.314,2.686,6,6,6h14V28h-4v-3c0-1.105,0.895-2,2-2h2v-3 c0-5,3-7,7-7c2.045,0,3,0.354,3,0.354V16c0,1.105-0.895,2-2,2h-0.518C31.676,18,31,19.07,31,21v2h5l-0.678,3.392 C35.135,27.327,34.314,28,33.36,28H31v14h5c3.314,0,6-2.686,6-6V12C42,8.686,39.314,6,36,6z"
-              ></path>
-              <linearGradient
-                id="iFd9NrbBteaozGC2ch1MZb_MNRKfKM11gxe_gr2"
-                x1="20.441"
-                x2="73.962"
-                y1="15.094"
-                y2="78.877"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop offset="0" stop-color="#262626" stop-opacity="0"></stop>
-                <stop offset="1" stop-color="#262626" stop-opacity=".8"></stop>
-              </linearGradient>
-              <path
-                fill="url(#iFd9NrbBteaozGC2ch1MZb_MNRKfKM11gxe_gr2)"
-                d="M31,21v2h5l-0.678,3.392C35.135,27.327,34.314,28,33.36,28H31v14h-5V28h-4v-3 c0-1.105,0.895-2,2-2h2v-3c0-5,3-7,7-7c2.045,0,3,0.354,3,0.354V16c0,1.105-0.895,2-2,2h-0.518C31.676,18,31,19.07,31,21z"
-              ></path>
-            </svg>
-          </a>
-          <a href="" class="hover:animate-pulse">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              width="100"
-              height="100"
-              viewBox="0 0 48 48"
-            >
-              <linearGradient
-                id="_zRckIf366dQufSrDA5IFa_91AZsA4BDGzd_gr1"
-                x1="-10.294"
-                x2="50.002"
-                y1="-10.294"
-                y2="50.002"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop offset="0" stop-color="#262626" stop-opacity="0"></stop>
-                <stop offset="1" stop-color="#262626" stop-opacity=".8"></stop>
-              </linearGradient>
-              <path
-                fill="url(#_zRckIf366dQufSrDA5IFa_91AZsA4BDGzd_gr1)"
-                d="M36,6H12c-3.314,0-6,2.686-6,6v24c0,3.314,2.686,6,6,6h24c3.314,0,6-2.686,6-6V12 C42,8.686,39.314,6,36,6z M17,36h-5V21c0-1.105,0.895-2,2-2h3V36z M15.122,16.925c-1.861,0.449-3.496-1.186-3.047-3.047 c0.211-0.875,0.928-1.592,1.803-1.803c1.861-0.448,3.495,1.186,3.047,3.047C16.714,15.997,15.997,16.714,15.122,16.925z M36,36h-3 c-1.105,0-2-0.895-2-2l0-6.83c0-1.624-1.216-3.081-2.839-3.166C26.431,23.914,25,25.29,25,27v9h-5V21c0-1.105,0.895-2,2-2h3v2 c0,0,1.5-2,4.5-2c3.866,0,6.5,2.768,6.5,7V36z"
-              ></path>
-              <linearGradient
-                id="_zRckIf366dQufSrDA5IFb_91AZsA4BDGzd_gr2"
-                x1="6.932"
-                x2="83.561"
-                y1="9.182"
-                y2="100.504"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop offset="0" stop-color="#262626" stop-opacity="0"></stop>
-                <stop offset="1" stop-color="#262626" stop-opacity=".8"></stop>
-              </linearGradient>
-              <path
-                fill="url(#_zRckIf366dQufSrDA5IFb_91AZsA4BDGzd_gr2)"
-                d="M14,19h3v17h-5V21C12,19.895,12.895,19,14,19z M14.5,12c-1.381,0-2.5,1.119-2.5,2.5 s1.119,2.5,2.5,2.5s2.5-1.119,2.5-2.5S15.881,12,14.5,12z M29.5,19c-3,0-4.5,2-4.5,2v-2h-3c-1.105,0-2,0.895-2,2v15h5l0-8.83 c0-1.624,1.216-3.081,2.839-3.166C29.569,23.914,31,25.29,31,27v7c0,1.105,0.895,2,2,2h3V26C36,21.768,33.366,19,29.5,19z"
-              ></path>
-            </svg>
-          </a>
-          <a href="" class="hover:animate-pulse">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              width="100"
-              height="100"
-              viewBox="0 0 48 48"
-            >
-              <linearGradient
-                id="PPS2EZlnmq9zQzR2uzDxia_1bj89U1Ansyb_gr1"
-                x1="-28.671"
-                x2="92.138"
-                y1="50.465"
-                y2="-19.284"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop offset="0" stop-color="#262626" stop-opacity="0"></stop>
-                <stop
-                  offset=".839"
-                  stop-color="#262626"
-                  stop-opacity=".671"
-                ></stop>
-                <stop offset="1" stop-color="#262626" stop-opacity=".8"></stop>
-              </linearGradient>
-              <path
-                fill="url(#PPS2EZlnmq9zQzR2uzDxia_1bj89U1Ansyb_gr1)"
-                d="M47,10.948c-1.648,0.73-3.421,1.217-5.291,1.448c1.901-1.129,3.364-2.929,4.05-5.056	c-1.779,1.043-3.75,1.809-5.847,2.213c-1.674-1.775-4.063-2.877-6.709-2.877c-5.083,0-9.203,4.084-9.203,9.119	c0,0.713,0.083,1.407,0.24,2.081c-7.647-0.384-14.429-4.019-18.966-9.536c-0.797,1.348-1.246,2.926-1.246,4.592	c0,3.166,1.621,5.953,4.093,7.595c-1.509-0.046-2.929-0.466-4.172-1.139c0,0.027,0,0.069,0,0.107c0,4.424,3.173,8.108,7.379,8.946	C10.56,28.653,9.747,28.77,8.91,28.77c-0.594,0-1.174-0.067-1.734-0.168c1.171,3.615,4.568,6.258,8.594,6.336	C12.62,37.38,8.653,38.843,4.34,38.843c-0.745,0-1.474-0.042-2.194-0.13c4.075,2.58,8.914,4.093,14.108,4.093	c16.925,0,26.184-13.898,26.184-25.956c0-0.395-0.012-0.789-0.031-1.177C44.213,14.397,45.77,12.787,47,10.948L47,10.948z"
-              ></path>
-            </svg>
-          </a>
+          <FacebookButton link="https://www.facebook.com/" />
+          <LinkedinButton link="https://www.linkedin.com/" />
+          <TwitterButton link="https://www.twitter.com/" />
         </div>
       </div>
     </div>
