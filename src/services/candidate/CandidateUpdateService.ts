4524cc728d1e5576f1bc937219a558961ce0e7d4
chado <a.sale@outlook.fr>
2024-01-05 11:34:51 +0100

    Add CandidateUpdateService for user updates
    Added a new service 'CandidateUpdateService' for updating user profiles. The service uses an axios 'put' request to update specific user details. This update returns a promise that depends on the response status, while any errors are logged.

---
+++ src/services/candidate/CandidateUpdateService.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { UserType } from "@/types/UserType";
+
+export const updateCandidate = async (user: UserType): Promise<boolean> => {
+  try {
+    const response = await axios.put(
+      `https://127.0.0.1:8000/candidate/update/${user.id}`,
+      user,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response.status === 200) {
+      return true;
+    }
+    return false;
+  } catch (error: any) {
+    console.log(error);
+    return false;
+  }
+};
