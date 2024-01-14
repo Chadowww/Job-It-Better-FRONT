ecade14b2d6be1af1a10810919b1e4743d6d235a
chado <a.sale@outlook.fr>
2024-01-14 07:28:19 +0100

    Add register modal test in login spec
    Introduced unit tests for 'CandidateModalRegister' in 'login.spec.ts' to ensure the correct behavior of registration process from the login modal. Also, improved existing tests by setting values for email field, thus covering more scenarios and potential edge cases.

--- tests/unit/login.spec.ts
+++ tests/unit/login.spec.ts
@@ -1,9 +1,11 @@
 import CandidateModalLogin from "@/components/molecules/candidate/CandidateModalLogin.vue";
+import CandidateModalRegister from "@/components/molecules/candidate/CandidateModalRegister.vue";
 import { userLogin } from "@/services/user/UserLoginService";
 import { mount } from "@vue/test-utils";
 import store from "@/store";
 
 jest.mock("@/services/user/UserLoginService.ts");
+
 describe("CandidateModalLogin", () => {
   it("should render", () => {
     const wrapper = mount(CandidateModalLogin, {
@@ -39,12 +41,15 @@ describe("CandidateModalLogin", () => {
 
     expect(wrapper.find(".error").text()).toBe("L'email n'est pas valide.");
   });
+
   it("should show error password", async () => {
     const wrapper = mount(CandidateModalLogin, {
       global: {
         plugins: [store],
       },
     });
+    const emailField = wrapper.find("input[type='email']");
+    await emailField.setValue("valid.email@example.com");
 
     const passwordField = wrapper.find("input[type='password']");
     await passwordField.setValue("fw7jzpdr7!");
@@ -52,5 +57,29 @@ describe("CandidateModalLogin", () => {
     expect(wrapper.find(".error").text()).toBe(
       "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial"
     );
+  });
+
+  it("should change modal", async () => {
+    const wrapper = mount(CandidateModalLogin, {
+      global: {
+        plugins: [store],
+      },
+    });
+    const wrapper2 = mount(CandidateModalRegister, {
+      global: {
+        plugins: [store],
+      },
+    });
+
+    const link = wrapper
+      .findAll("a")
+      .filter((w) => w.text() === "Pas encore inscrit ?")[0];
+
+    await link.trigger("click");
+    expect(wrapper2.exists()).toBe(true);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
   });
 });
