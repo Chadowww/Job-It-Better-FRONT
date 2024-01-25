0cacd8647e16fe782dae1099eff32aded0620597
chado <a.sale@outlook.fr>
2024-01-25 12:42:16 +0100

    Add token check in UserService getUser method
    Added a condition to check if the user token exists before making the API call in the UserService getUser method. This will optimize the function by not making unnecessary requests and preventing potential failures due to missing tokens.

--- src/services/API/user/UserService.ts
+++ src/services/API/user/UserService.ts
@@ -7,16 +7,21 @@ export const user = ref(<UserType>{});
 
 export const getUser = async (): Promise<UserType | boolean> => {
   try {
-    const response = await Axios.get(
-      "https://127.0.0.1:8000/user/get-from-token",
-      {
-        headers: {
-          Authorization: `Bearer ${token.value}`,
-        },
-      }
-    );
-    user.value = response.data;
-    return user.value;
+    if (token.value) {
+      const response = await Axios.get(
+        "https://127.0.0.1:8000/user/get-from-token",
+        {
+          headers: {
+            Authorization: `Bearer ${token.value}`,
+          },
+        }
+      );
+
+      user.value = response.data;
+      return user.value;
+    } else {
+      return false;
+    }
   } catch (error) {
     console.log(error);
     return false;
