0dd89059603dcf5af549c0b02a775dc4f083fc43
chado <a.sale@outlook.fr>
2024-01-29 13:16:10 +0100

    Create new JobOfferType interface
    This commit introduces a new TypeScript interface named JobOfferType. This interface is defined with multiple properties to match with the Job Offer related data structure. This will ensure that the job offer data representation aligns seamlessly across the application.

---
+++ src/types/JobOfferType.ts
@@ -0,0 +1,22 @@
+export interface JobOfferType {
+  jobofferId: number;
+  title: string;
+  description: string;
+  city: string;
+  salaryMin: number;
+  salaryMax: number;
+  contractId: number;
+  company_description: string;
+  companyId: number;
+  categoryId: number;
+  name: string;
+  phone: string;
+  address: string;
+  country: string;
+  siret: string;
+  logo: string;
+  slug: string;
+  cover: string;
+  userId: number;
+  type: string;
+}
