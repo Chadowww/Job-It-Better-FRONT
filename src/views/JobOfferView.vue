b4ae8e443457413cf784ac8e27522971d2673a76
chado <a.sale@outlook.fr>
2024-01-29 13:16:19 +0100

    Add new JobOfferView.vue component
    This commit adds a new component, JobOfferView, which includes implementation of the MainJobOfferComponent. The component is designed with flexibility in mind, being built using flexbox for easy adaptability across different sections of the application.

---
+++ src/views/JobOfferView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MainJobOfferComponent from "@/components/organisms/JobOffer/MainJobOfferComponent.vue";
+</script>
+
+<template>
+  <main class="flex flex-col justify-between">
+    <MainJobOfferComponent />
+  </main>
+</template>
+
+<style scoped lang="scss"></style>
