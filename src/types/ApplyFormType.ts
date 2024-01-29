64223a9f9bf49d05cb5fda81e18a2a44eb310659
chado <a.sale@outlook.fr>
2024-01-29 13:15:06 +0100

    Add ApplyFormType interface
    A new interface named ApplyFormType is created in src/types. This interface includes properties such as candidateId, resumeId, jobofferId, status, message, createdAt, and updatedAt which all can be in the form of a number or string or may be null.

---
+++ src/types/ApplyFormType.ts
@@ -0,0 +1,9 @@
+export interface ApplyFormType {
+  candidateId: number | null;
+  resumeId: number | null;
+  jobofferId: number | null;
+  status: string | null;
+  message: string | null;
+  createdAt: string | null;
+  updatedAt: string | null;
+}
